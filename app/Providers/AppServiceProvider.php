43f952bf14ee21fd8ba4fb52e1ce3adef6cb405e
Thang Le <thang.le@sapotacorp.vn>
2024-08-03 15:55:58 +0700

    gf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
@@ -24,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
