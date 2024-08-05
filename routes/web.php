ae1f398a191a6704847eaf4c1271c61190e7e3d6
Thang Le <thang.le@sapotacorp.vn>
2024-08-05 13:59:54 +0700

    dgd

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::group(['namespace' => 'Api\V1'], function () {
     Route::group(['prefix' => 'products'], function () {
-        Route::get('popular', 'ProductController@get_popular_products');
+        Route::get('popular', [ProductController::class,'get_popular_products']);
         Route::get('recommended', 'ProductController@get_recommended_products');
         // Route::get('drinks', 'ProductController@get_drinks');
         Route::get('test', 'ProductController@test_get_recommended_products');
